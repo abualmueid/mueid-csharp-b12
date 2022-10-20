e0301d84156e655c3bc5063bed7b6a6662fe6957
abualmueid <61595430+abualmueid@users.noreply.github.com>
2022-10-20 16:01:06 +0600

    added class 7 practise contents

---
+++ String Operations/Program.cs
@@ -0,0 +1,15 @@
+﻿char c = 'l';
+int x = (int)c;
+Console.WriteLine(x);
+
+char y = (char)x;
+Console.WriteLine(y);
+
+string name = "Abu Al Mueid";
+name = name.Replace("A", "B");
+Console.WriteLine(name);
+
+int pos = name.IndexOf('M');
+Console.WriteLine(pos);
+
+Console.WriteLine(char.IsPunctuation('e'));
