d8168e1af78f4fc19f7101fef21e8514363aff0a
abualmueid <61595430+abualmueid@users.noreply.github.com>
2022-10-24 17:26:59 +0600

    modified Program.cs

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OOP;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        WaterBottle bottle = new WaterBottle("red");
+        bottle.waterAmount = 300;
+        bottle.AddWater(200);
+
+        WaterBottle bottle2 = new WaterBottle("blue");
+        bottle2.waterAmount = 800;
+        bottle2.AddWater(200);
+
+        Console.WriteLine(bottle.waterAmount);
+        Console.WriteLine(bottle2.waterAmount);
+        Console.WriteLine(bottle.GetColor());
+        Console.WriteLine(bottle2.GetColor());
+    }
+}
