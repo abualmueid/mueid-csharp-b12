879449687d02353946c14e1b174235533a80b6ab
abualmueid <61595430+abualmueid@users.noreply.github.com>
2022-10-24 17:32:43 +0600

    added WaterBottle.cs

---
+++ OOP/WaterBottle.cs
@@ -0,0 +1,24 @@
+﻿namespace OOP
+{
+    internal class WaterBottle
+    {
+        internal double waterAmount;
+        internal string color;
+
+        internal WaterBottle(string color)
+        {
+            this.color = color;
+        }
+
+        internal void AddWater(double amount)
+        {
+            waterAmount += amount;
+        }
+
+        // Get Method
+        internal string GetColor()
+        {
+            return color;
+        }
+    }
+}
