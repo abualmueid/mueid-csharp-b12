2378112017764e69bd150a91d3aadbe01790dd15
abualmueid <61595430+abualmueid@users.noreply.github.com>
2022-11-07 16:44:18 +0600

    modified

--- Object Oriented Programming/OOPTest/Program.cs
+++ Object Oriented Programming/OOPTest/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OOPTest;
+
+VVIPMember member = new VVIPMember(12345);
+member.Name = "Abu Al Mueid";
