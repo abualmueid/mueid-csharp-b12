8b79e1d03a358891d927c7637528b69af9724eed
abualmueid <61595430+abualmueid@users.noreply.github.com>
2022-11-09 14:19:42 +0600

    Assignment3 folder and files added

---
+++ Assignment3/Task1/Product.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Task1
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string Color { get; set; }
+
+        public Product() : this("Potato", 10, "Chips", "Yellow")
+        {
+            
+        }
+
+        public Product(string name, double price) : this()
+        {
+            Name = name;
+            Price = price;
+        }
+
+         
+
+        public Product(string name, double price, string description, string color)
+        {
+            Name = name;
+            Price = price;
+            Description = description;
+            Color = color;
+
+            Console.WriteLine("2nd constructor");
+        }
+
+
+
+    }
+}
---
+++ Assignment3/Task1/Program.cs
@@ -0,0 +1,7 @@
+﻿using Task1;
+
+//Product product = new Product("Astrum", 300, "Mouse", "Black");
+Product product = new Product("Astrum", 300);
+//Product product = new Product();
+//product.Name = "Mueid";
+Console.WriteLine(product.Name);
