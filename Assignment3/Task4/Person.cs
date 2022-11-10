a3fd2e2a9a08dd0c8c3572699af2eb84ab354b06
abualmueid <61595430+abualmueid@users.noreply.github.com>
2022-11-10 14:23:24 +0600

    added a file

---
+++ Assignment3/Task4/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    public abstract class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Id { get; protected set; }
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+
+        public Person(string firstName, string lastName, string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+        }
+
+        public abstract void GenerateId();
+        
+    }
+}
