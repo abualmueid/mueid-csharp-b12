0fc6dfc15983c31d935d32a37d601e0ec16cc60b
abualmueid <61595430+abualmueid@users.noreply.github.com>
2022-11-23 22:51:18 +0600

    solved 112A

---
+++ Assignment2/112A/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string str1, str2;
+
+str1 = Console.ReadLine();
+str2 = Console.ReadLine();
+
+str1 = str1.ToLower();
+str2 = str2.ToLower();
+
+Console.WriteLine(string.Compare(str1, str2));
