da6197e12a56cfab38dc12688b0f04d53f6f1a50
abualmueid <61595430+abualmueid@users.noreply.github.com>
2022-11-27 16:26:27 +0600

    learned Enum

---
+++ Class13/Enum/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Enum;
+
+Console.WriteLine("Hello, World!");
+
+User user = new User();
+user.Name = "abualmueid";
+user.Email = "abualmueid24@gmail.com";
+Console.WriteLine(user.Name);
+Console.WriteLine(user.Email);
+Console.WriteLine(UserStatus.Active);
---
+++ Class13/Enum/UserStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum
+{
+    internal enum UserStatus
+    {
+        Active = 0,
+        Inactive = 1,
+        Pending = 2,
+        Deleted = 3
+    }
+}
