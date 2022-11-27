4d2d22394399fde41ea3c9072062c128cc84b13e
abualmueid <61595430+abualmueid@users.noreply.github.com>
2022-11-27 16:52:56 +0600

    learned StringBuilder

---
+++ Class13/StringBuilder/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+string location = "Dhaka";
+location += ", Bangladesh";
+Console.WriteLine(location);
+
+StringBuilder stringBuilder = new StringBuilder(location);
+stringBuilder.Append(", Asia");
+Console.WriteLine(stringBuilder);
+
+stringBuilder.Replace('a', 'A');
+Console.WriteLine(stringBuilder);
+
+stringBuilder.Insert(5, '*');
+Console.WriteLine(stringBuilder);
+
+stringBuilder[0] = '#';
+Console.WriteLine(stringBuilder);
