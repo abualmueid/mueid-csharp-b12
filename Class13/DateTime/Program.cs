58a564e6b1b87d4a5cf852f1fa36f6d35c1c0eed
abualmueid <61595430+abualmueid@users.noreply.github.com>
2022-11-27 16:26:51 +0600

    learned DateTime

---
+++ Class13/DateTime/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+DateTime dateTime = new DateTime(2022, 11, 27);
+Console.WriteLine(dateTime);
+
+dateTime = dateTime.AddMonths(2);
+Console.WriteLine(dateTime);
+
+DateTime dateTime2 = DateTime.UtcNow;
+Console.WriteLine(dateTime2);
+
+TimeSpan timeSpan = dateTime2.Subtract(dateTime);
+Console.WriteLine(timeSpan.TotalDays);
