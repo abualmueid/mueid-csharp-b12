cdf49558fe1be711d43b364d8ed0975eb4540419
abualmueid <61595430+abualmueid@users.noreply.github.com>
2022-11-27 18:33:55 +0600

    learned Parameter Modifier

---
+++ Class13/ParameterModifier/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// double avg = CalculateAvg(new double[] {2, 3, 4, 5, 6});
+double avg = CalculateAvg(2, 3, 4, 5, 6);
+
+double CalculateAvg(params double[] numbers)
+{
+    double sum = 0;
+    foreach (double number in numbers)
+        sum += number;
+    
+    return sum/numbers.Length;
+}
+
+void UpdateValue1(in double value)
+{
+    // value = 5; // you cannot assign values here. readonly
+}
+
+void UpdateValue2(out double value)
+{
+    value = 5; // you can assign/modifies values
+}
+
+void UpdateValue3(ref double value)
+{
+    value = 5;
+    value++;
+}
+
+double x = 3;
+UpdateValue3(ref x);
+Console.WriteLine(x);
