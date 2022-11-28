999594d214a37bc4dfab4164d671e063470e641e
abualmueid <61595430+abualmueid@users.noreply.github.com>
2022-11-28 11:38:04 +0600

    practiced Class12

---
+++ Class12/NewAndSealedKeyword/BigNumber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewAndSealedKeyword
+{
+    internal class BigNumber : Number
+    {
+        
+        public sealed override void SetNumber()
+        {
+
+        }
+    }
+}
