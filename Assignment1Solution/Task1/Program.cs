539e82a683678c758bc3b7d8cc2d1192da049c3a
abualmueid <61595430+abualmueid@users.noreply.github.com>
2022-11-28 11:36:44 +0600

    revised Assignment1 Solution

---
+++ Assignment1Solution/Task1/Program.cs
@@ -0,0 +1,62 @@
+﻿Console.WriteLine("Enter Size of rows:");
+int rows = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter Size of columns:");
+int cols = int.Parse(Console.ReadLine());
+
+int[,] array = new int[rows, cols];
+
+for (int i = 0; i < rows; i++)
+{
+    string[] nums = Console.ReadLine().Split(' ');
+    for(int j= 0; j < cols && j < nums.Length; j++)
+    {
+        array[i, j] = int.Parse(nums[j]);
+    }
+}
+
+int max = int.MinValue;
+// row sum
+for(int i = 0; i < rows; i++)
+{
+    int sum = 0;
+    for(int j = 0; j < cols; j++)
+    {
+        sum += array[i, j];
+    }
+    if (sum > max)
+        max = sum;
+}
+
+// column sum
+for (int i = 0; i < cols; i++)
+{
+    int sum = 0;
+    for (int j = 0; j < rows; j++)
+    {
+        sum += array[j, i];
+    }
+    if (sum > max)
+        max = sum;
+}
+
+// diagonal sum
+if (rows == cols)
+{
+    int diag1Sum = 0;
+    int diag2Sum = 0;
+    for (int i = 0; i < rows; i++)
+    {
+        diag1Sum += array[i, i];
+        diag2Sum += array[i, rows - i - 1];
+    }
+
+    if (diag1Sum > max)
+        max = diag1Sum;
+
+    if (diag2Sum > max)
+        max = diag2Sum;
+}
+
+Console.WriteLine($"Max value is: {max}");
+
+
---
+++ Assignment1Solution/Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter how many numbers you want to input:");
+int size = int.Parse(Console.ReadLine());
+
+int firstMax = int.MinValue;
+int secondMax = int.MinValue;
+int thirdMax = int.MinValue;
+
+for (int i = 0; i < size; i++)
+{
+    int num = int.Parse(Console.ReadLine());
+
+    if (num > firstMax)
+    {
+        thirdMax = secondMax;
+        secondMax = firstMax;
+        firstMax = num;
+    }
+    else if (num > secondMax)
+    {
+        thirdMax = secondMax;
+        secondMax = num;
+    }
+    else if (num > thirdMax)
+        thirdMax = num;
+}
+
+Console.WriteLine(thirdMax);
