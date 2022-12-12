41530ee9631b08091f63d1b2d70526e3f46e382b
abualmueid <61595430+abualmueid@users.noreply.github.com>
2022-12-12 10:13:45 +0600

    added files

--- Class17/ADODotNet/DataUtility.cs
+++ Class17/ADODotNet/DataUtility.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,29 @@ namespace ADODotNet
 {
     public class DataUtility
     {
+        public void InsertData()
+        {
+            //string connectionString = @"Server=DESKTOP-SJFJ7LE\SQLEXPRESS01;Database=CSharpB12;User Id=DESKTOP-SJFJ7LE\Abu Al Mueid;Password=;";
+            string connectionString = @"Server=DESKTOP-SJFJ7LE\SQLEXPRESS01;Database=CSharpB12;Trusted_Connection=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            SqlCommand command = connection.CreateCommand();
+            command.Connection = connection;
+
+            //command.CommandText = "insert into Courses(Title, Fees, ClassStartDate, IsOpen, CourseCode) values('Node.js', 6000, '10-1-2023', 1, '1CC7D510-8130-4248-9947-BA2B3083B9EB')";
+            string commandText = "insert into Courses(Title, Fees, ClassStartDate, IsOpen, CourseCode) values('Node.js', 6000, '10-1-2023', 1, '1CC7D510-8130-4248-9947-BA2B3083B9EB')";
+            command.CommandText = commandText;
+
+            
+
+            if (connection.State != System.Data.ConnectionState.Open)
+                connection.Open();
+
+            command.ExecuteNonQuery();
+            connection.Close();
+            command.Dispose();
+        }
+
 
     }
 }
--- Class17/ADODotNet/Program.cs
+++ Class17/ADODotNet/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ADODotNet;
+
 Console.WriteLine("Hello, World!");
+
+DataUtility datautility = new DataUtility();
+datautility.InsertData();
